a8dd351ab9823815b8795e04e7bb26baa8decb87
Ivana Lieska <idrabikova@gmail.com>
2016-12-28 18:50:38 +0100

    removed generated files

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,15 @@
 (function() {
   'use strict';
-  angular.module('myFirstApp', [])
-    .controller('MyFirstController', function () {
-
+  angular.module('foxApp', [])
+    .controller('FoxController', function ($scope) {
+     $scope.foxes = [
+        {
+          src: "https://s-media-cache-ak0.pinimg.com/736x/7e/fd/79/7efd7995deb5d88c7baeda5dccd9e31a.jpg"
+        },
+        {
+          src: "https://s-media-cache-ak0.pinimg.com/originals/ac/09/a6/ac09a6c4cf1096277e1295c5db9bc70e.jpg"
+        }
+      ];
     });
 
 })();
