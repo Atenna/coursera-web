ac374c9015833eb5ace4f37508d17b4d548f356b
Ivana Lieska <idrabikova@gmail.com>
2016-12-30 23:49:02 +0100

    changing color :art:

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,18 @@
 (function() {
   'use strict';
   angular.module('foxApp', [])
-    .controller('FoxController', function ($scope) {
-     $scope.foxes = [
-        {
-          src: "https://s-media-cache-ak0.pinimg.com/736x/7e/fd/79/7efd7995deb5d88c7baeda5dccd9e31a.jpg"
-        },
-        {
-          src: "https://s-media-cache-ak0.pinimg.com/originals/ac/09/a6/ac09a6c4cf1096277e1295c5db9bc70e.jpg"
-        }
-      ];
-    });
+    .controller('FoxController', FoxController);
 
+  FoxController.$inject = ['$scope']; //prevents minifiers from breaking the code
+  function FoxController ($scope) {
+    $scope.active = true;
+    $scope.foxes = [
+      {
+        src: "https://s-media-cache-ak0.pinimg.com/736x/7e/fd/79/7efd7995deb5d88c7baeda5dccd9e31a.jpg"
+      },
+      {
+        src: "https://s-media-cache-ak0.pinimg.com/originals/ac/09/a6/ac09a6c4cf1096277e1295c5db9bc70e.jpg"
+      }
+    ];
+  }
 })();
